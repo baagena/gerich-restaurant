117f92d933464c442e2562df7b1e9450cbd08a07
baagena <baagena@gmail.com>
2024-08-10 21:03:01 +0200

    Build hero section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
-
+import "./Header.css";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="header">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The key to Fine Dining</h1>
+    </div>
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
